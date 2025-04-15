6cbaddf3f9a8bd014948efcf026d44e17361035e
Vishwarenuka Boli <113835874+vboli@users.noreply.github.com>
2025-04-15 10:52:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import langchain_helper
 
-
 st.title("Restaurant Name Generator")
 
 cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
